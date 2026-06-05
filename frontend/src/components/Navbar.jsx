db58b03a62e0cb74ae2b3a9d58606ffe097edac9
Ronak Sharma <sharmaronak2908@gmail.com>
2026-06-06 00:59:52 +0530

    Delete frontend directory

--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Navbar = () => (
-  <nav className="navbar navbar-dark bg-dark px-3">
-    <span className="navbar-brand mb-0 h1">Internship Task Dashboard</span>
-  </nav>
-);
-
-export default Navbar;
